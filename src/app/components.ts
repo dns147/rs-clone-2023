2345aa34bcd544ffaa08c1c5c584379b052f9da2
tatiana <miss.lacrima.ts@gmail.com>
2023-02-07 03:03:31 +0300

    fix: change footer rendering

--- src/app/components.ts
+++ src/app/components.ts
@@ -37,26 +37,35 @@ export const Content = {
   },
 };
 
+// export const Footer = {
+//   render: () => {
+//     return `
+//       <footer class="footer">
+//         <ul class="footer-list">
+//           <li class="footer-item item-github">
+//             <img src="https://svgshare.com/i/og2.svg" width="50" alt="icon" class="github-link">
+//             <a href="https://github.com/dns147" class="footer-link" target="_blank">Denis Zhuravlev</a>
+// 						<a href="https://github.com/Tatiana-Shylovich" class="footer-link" target="_blank">Tatiana Shylovich</a>
+// 						<a href="https://github.com/avshir" class="footer-link" target="_blank">Anna Shirinskaya</a>
+//           </li>
+//           <li class="footer-item">
+//             <span>2023</span>
+//           </li>
+//           <li class="footer-item">
+//             <a href="https://rs.school/js/" target="_blank">
+//               <img src="https://rs.school/images/rs_school_js.svg" width="60" alt="icon" class="rs-link">
+//             </a>
+//           </li>
+//         </ul>
+//       </footer>
+//     `;
+//   },
+// };
+
 export const Footer = {
   render: () => {
     return `
       <footer class="footer">
-        <ul class="footer-list">
-          <li class="footer-item item-github">
-            <img src="https://svgshare.com/i/og2.svg" width="50" alt="icon" class="github-link">
-            <a href="https://github.com/dns147" class="footer-link" target="_blank">Denis Zhuravlev</a>
-						<a href="https://github.com/Tatiana-Shylovich" class="footer-link" target="_blank">Tatiana Shylovich</a>
-						<a href="https://github.com/avshir" class="footer-link" target="_blank">Anna Shirinskaya</a>
-          </li>
-          <li class="footer-item">
-            <span>2023</span>
-          </li>
-          <li class="footer-item">
-            <a href="https://rs.school/js/" target="_blank">
-              <img src="https://rs.school/images/rs_school_js.svg" width="60" alt="icon" class="rs-link">
-            </a>
-          </li>
-        </ul>
       </footer>
     `;
   },
