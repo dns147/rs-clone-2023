6b143dc28c07cc67725f5c4cac05319acbcbaa05
avshir <av.shirinskaya@gmail.com>
2023-02-17 15:06:08 +0100

    fix: delete unnecessary data in coreConst.ts

--- src/spa/coreConst.ts
+++ src/spa/coreConst.ts
@@ -7,20 +7,7 @@ const soundSettings = new Audio(PLAY_LIST_URL[0]);
 localStorage.setItem('isMusic', JSON.stringify(true));
 localStorage.setItem('isSoundEffects', JSON.stringify(true));
 
-const PLAY_LIST = {
-  music: [
-    'https://github.com/dns147/mp3-rs-clone-2023/raw/main/mp3/music/strashnye-zvuki-vorony.mp3',
-    'https://github.com/dns147/mp3-rs-clone-2023/raw/main/mp3/music/strashnye-zvuki-child-song.mp3',
-  ],
-  soundEffects: [
-    'https://github.com/dns147/mp3-rs-clone-2023/raw/main/mp3/effects/ice-boom.mp3',
-    'https://github.com/dns147/mp3-rs-clone-2023/raw/main/mp3/effects/settings-1.mp3',
-    'https://github.com/dns147/mp3-rs-clone-2023/raw/main/mp3/effects/fail-wha-wha.mp3',
-  ],
-};
-
 export default {
   PLAY_LIST_URL,
   soundSettings,
-  PLAY_LIST,
 };
