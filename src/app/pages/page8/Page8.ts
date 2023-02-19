b1915e763c47c944d372736b1b5d79186731a4e2
tatiana <miss.lacrima.ts@gmail.com>
2023-02-19 18:01:47 +0300

    feat: add basic game logic for parallax game

--- src/app/pages/page8/Page8.ts
+++ src/app/pages/page8/Page8.ts
@@ -1,9 +1,31 @@
+import './style-page8.scss';
+
+import { createBackground, createPlayer, setCanvas, animate, timer } from './utils-page8';
+
 export default class Page8 {
   constructor() {}
 
   render(): string {
-    return `Page8`;
+    return `<div class="parallax-game">
+      <div class="parallax-game-wrapper">
+        <div class="parallax-game-results">
+          <div class="parallax-game-results-time">
+            <span class="parallax-game-results-time__text">Time:</span>
+            <span class="parallax-game-results-time__number">00:00</span>
+          </div>
+        </div>
+        <div class="canvas-container">
+          <canvas id="canvas-parallax"></canvas>
+        </div>
+      </div>
+    </div>`;
   }
 
-  init(): void {}
+  init(): void {
+    setCanvas();
+    createBackground();
+    createPlayer();
+    animate(0);
+    timer();
+  }
 }
