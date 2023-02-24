526942e06e264781d3d942d435d0331e5e53be94
tatiana <miss.lacrima.ts@gmail.com>
2023-02-25 01:23:47 +0300

    feat: add page about game

--- src/app/pages/page2/Page2.ts
+++ src/app/pages/page2/Page2.ts
@@ -1,9 +1,117 @@
+import './style-page2.scss';
+// import { start } from './utils-page2';
+
 export default class Page2 {
   constructor() {}
 
   render(): string {
-    return `Page2`;
+    return `
+    <div class="about-game">
+      <div class="canvas-stars-container">
+        <canvas id="canvas-stars"></canvas>
+      </div>
+      <div class="about-game-content">
+        <div class="games-info">
+          <div class="game-info-container">
+            <div class="game-info-heading">
+              <div class="game-icon-container">
+                <img class="game-icon-container__img" src=${require('../../../assets/map/skull3.png')} alt="skull">
+              </div>
+              <h2 class="game-heading">Cemetery</h2>
+            </div>
+            <div class="game-info-description">
+              <p>Open coffins and find matches. Have some fun before real challenges!</p>
+              <p>Levels: 2.</p>
+              <p>Controls: click...if you dare.</p>
+            </div>
+          </div>
+          <div class="game-info-container">
+            <div class="game-info-heading">
+              <div class="game-icon-container">
+                <img class="game-icon-container__img" src=${require('../../../assets/map/raven.png')} alt="raven">
+              </div>
+              <h2 class="game-heading">Ravens hunting</h2>
+            </div>
+            <div class="game-info-description">
+              <p>Bla-bla-bla</p>
+              <p>Levels: 1.</p>
+              <p>Controls: click...if you dare.</p>
+           </div>
+          </div>
+          <div class="game-info-container">
+            <div class="game-info-heading">
+              <div class="game-icon-container">
+                <img class="game-icon-container__img" src="" alt="">
+              </div>
+              <h2 class="game-heading">Zombie walk</h2>
+            </div>
+            <div class="game-info-description">
+              <p>Bla-bla-bla</p>
+              <p>Levels: ??.</p>
+              <p>Controls: ???.</p>
+            </div>
+          </div>
+          <div class="game-info-container">
+            <div class="game-info-heading">
+            <div class="game-icon-container">
+              <img class="game-icon-container__img" src=${require('../../../assets/map/pumpkin.png')} alt="pumpkin">
+            </div>
+            <h2 class="game-heading">Save pumpkin</h2>
+          </div>
+          <div class="game-info-description">
+            <p>Bla-bla-bla</p>
+            <p>Levels: ???.</p>
+            <p>Controls: ???.</p>
+          </div>
+        </div>
+        </div>
+        <div class="creators-info">
+          <div class="creator">
+            <h3 class="creator-name">Denis</h3>
+            <ul class="duties-list">
+              <li class="duties-list__item">team lead</li>
+              <li class="duties-list__item">autorization</li>
+              <li class="duties-list__item">save pumpkin</li>
+            </ul>
+            <a class="github-link" href="https://github.com/dns147" target="_blank">
+              <div class="github-link__container">
+                <img class="github-link__img" src=${require('../../../assets/img/github-logo.png')} alt="github">
+              </div>
+            </a>
+          </div>
+          <div class="creator">
+            <h3 class="creator-name">Tatiana</h3>
+            <ul class="duties-list">
+              <li class="duties-list__item">design</li>
+              <li class="duties-list__item">cemetery</li>
+              <li class="duties-list__item">zombie walk</li>
+            </ul>
+            <a class="github-link" href="https://github.com/Tatiana-Shylovich" target="_blank">
+              <div class="github-link__container">
+                <img class="github-link__img" src=${require('../../../assets/img/github-logo.png')} alt="github">
+              </div>
+            </a>
+          </div>
+          <div class="creator">
+            <h3 class="creator-name">Anna</h3>
+            <ul class="duties-list">
+              <li class="duties-list__item">music</li>
+              <li class="duties-list__item">results</li>
+              <li class="duties-list__item">ravens hunting</li>
+            </ul>
+            <a class="github-link" href="https://github.com/avshir" target="_blank">
+              <div class="github-link__container">
+                <img class="github-link__img" src=${require('../../../assets/img/github-logo.png')} alt="github">
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
   }
 
-  init(): void {}
+  init(): void {
+    // setTimeout(start, 300);
+  }
 }
