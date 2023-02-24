86d301de5a9a6cf936c83a072696d1a512446ec9
avshir <av.shirinskaya@gmail.com>
2023-02-25 00:53:13 +0100

    fix: update component modal.scss, modalTemplates.ts

--- src/app/pages/modal/modalTemplates.ts
+++ src/app/pages/modal/modalTemplates.ts
@@ -7,9 +7,8 @@ const modalTemplateSettings = `
       </div>
         <button class="settings__btn settings__btn--music btn btn--col-3 accent-font music-btn"><span></span> Music on/off</button>
         
-        <a href="#/chooseGames" class="settings__btn btn accent-font accent-font--upper">play games</a>
+        <a href="#/chooseGames" class="settings__btn btn accent-font accent-font--upper">games</a>
         <a href="#/results" class="settings__btn btn accent-font accent-font--upper">results</a>
-        <a href="#" class="settings__btn btn accent-font accent-font--upper">On main page</a>
       </div>
   </div>
 `;
