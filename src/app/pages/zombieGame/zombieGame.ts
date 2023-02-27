c1311ee27e2f1b896ede93aa4b75f97af874289a
tatiana <miss.lacrima.ts@gmail.com>
2023-02-27 06:01:33 +0300

    feat: add modal message and change logic to start game

--- src/app/pages/zombieGame/zombieGame.ts
+++ src/app/pages/zombieGame/zombieGame.ts
@@ -1,6 +1,14 @@
 import './style-zombie-game.scss';
-
-import { createBackground, createPlayer, setCanvas, animate, timer } from './utils-zombie-game';
+import ModalMessageTemplates from '../modalMessage/modalMessageTemplates';
+import {
+  createBackground,
+  createPlayer,
+  setCanvasSize,
+  animate,
+  timer,
+  getEnemyTime,
+  setInitialValues,
+} from './utils-zombie-game';
 
 export default class Page8 {
   constructor() {}
@@ -8,24 +16,43 @@ export default class Page8 {
   render(): string {
     return `<div class="parallax-game">
       <div class="parallax-game-wrapper">
-        <div class="parallax-game-results">
+        <div class="canvas-container">
+          <canvas id="canvas-parallax"></canvas>
+          <div class="parallax-game-results">
           <div class="parallax-game-results-time">
             <span class="parallax-game-results-time__text">Time:</span>
             <span class="parallax-game-results-time__number">00:00</span>
           </div>
         </div>
-        <div class="canvas-container">
-          <canvas id="canvas-parallax"></canvas>
         </div>
+        ${ModalMessageTemplates.startGameBtnTemplate}
       </div>
     </div>`;
   }
 
   init(): void {
-    setCanvas();
+    window.addEventListener('click', this.handlerGame.bind(this));
+
+    setCanvasSize();
     createBackground();
     createPlayer();
     animate(0);
+  }
+
+  handlerGame(event: MouseEvent) {
+    const currEl = event.target;
+    const shooterGameArea = document.querySelector('.parallax-game') as HTMLElement;
+    const startGameBtn = shooterGameArea.querySelector('.start-game-btn') as HTMLElement;
+
+    if (currEl === startGameBtn) {
+      this.initGame();
+      startGameBtn.classList.add('hide');
+    }
+  }
+
+  initGame() {
+    setInitialValues();
     timer();
+    getEnemyTime();
   }
 }
